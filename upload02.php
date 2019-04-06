82b3924a6759015b414e26d2c970834952834f25
abpopko <47129962+abpopko@users.noreply.github.com>
2019-04-06 14:03:45 -0400

    Update upload02.php

--- upload02.php
+++ upload02.php
@@ -2,15 +2,10 @@
 	<body>
 	<div style='text-align: center;'>
 	<h2>Upload Profile Picture</h2>
-		<form method="post" enctype="multipart/form-data">
-			<table style='margin-left: 45%'><tr><td>
-				<input type="hidden" name="MAX_FILE_SIZE" value="1000000">
-				<input name="userfile" type="file" id="userfile">
-				</td></tr>
-				<tr>
-				<td><input name="upload" type="submit" id="upload"  value=" Upload "></td></tr>
-			</table>
-		</form>
+<form action="upload02.php" method="post" enctype="multipart/form-data">
+         <input type="file" name="file">
+         <button type="submit" name="upload_btn">Upload</button>
+       </form>
 		</div>
 	</body>
 </html>
@@ -18,43 +13,9 @@
 <?php
 require "database.php";
 require "customer.class.php";
-ini_set('file-uploads',true);
-if(isset($_POST['upload']) && $_FILES['userfile']['size']>0)
-{
-  $id = $_GET['id'];
-  $fileName = $_FILES['userfile']['name'];
-  $tmpName  = $_FILES['userfile']['tmp_name'];
-  $fileSize = $_FILES['userfile']['size'];
-  $fileType = $_FILES['userfile']['type'];
-  $fileType = (get_magic_quotes_gpc()==0 
-    ? mysql_real_escape_string($_FILES['userfile']['type'])
-    : mysql_real_escape_string(stripslashes ($_FILES['userfile'])));
-  $fp       = fopen($tmpName, 'r');
-  $content  = fread($fp, filesize($tmpName));
-  $content  = addslashes($content);
-  echo "<div style='text-align: center;'>filename: " . $fileName . "<br />";
-  echo "filesize: " . $fileSize . "<br />";
-  echo "filetype: " . $fileType . "<br />";
-fclose($fp);
-  
-   if (! get_magic_quotes_gpc() )
-   {
-     $fileName = addslashes($fileName);
-   }
-    $pdo = Database::connect();
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "UPDATE customers set profile_picture = ? WHERE id = ?";
-    $q = $pdo->prepare($sql);
-    $q->execute(array($content, $id));
-     
-	
-	echo "<br />File $fileName <br />uploaded successfully <br />";
-	
-	while ($row = mysql_fetch_assoc($result)) 
-{
-  echo "<p>" . $row['id'] . ' ' . $row['name'] . 
-    ' ' . $row['size'] . ' ' . $row['type'] . "</p></div>";
-}
+
+// call the uploadFile function
+customer:uploadFile();
 
 }      	
 ?>
